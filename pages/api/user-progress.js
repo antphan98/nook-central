339bac05150f1c51106a50fdc7526f324df2db97
An Phan <antphan98@gmail.com>
2020-06-11 18:43:24 -0500

    added firestore with get and post routes

---
+++ pages/api/user-progress.js
@@ -0,0 +1,46 @@
+import commonMiddleware from '../../utils/middleware/commonMiddleware';
+import { db } from '../../utils/auth/firebaseAdmin';
+
+const handler = async (req, res) => {
+  // Destroy the session.
+  // https://github.com/expressjs/cookie-session#destroying-a-session
+  console.log(req.session.decodedToken);
+  if (req.method === 'GET') {
+    let docRef = db
+      .collection('userProgress')
+      .doc(req.session.decodedToken.user_id);
+    const result = await docRef
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          const data = doc.data();
+          console.log('Document data:', data);
+          return data;
+        }
+      })
+      .catch((err) => {
+        console.log('Error getting document', err);
+      });
+    res.status(200).json({ data: result });
+  }
+
+  if (req.method === 'POST') {
+    req.session = {
+      decodedToken: {
+        user_id: '3K3jv5dcp1MKOGxIDSR1NYZDrGj1',
+      },
+    };
+
+    console.log(req.body);
+    let docRef = db
+      .collection('userProgress')
+      .doc(req.session.decodedToken.user_id);
+
+    docRef.set(req.body);
+    res.status(200).json({ status: true });
+  }
+};
+
+export default commonMiddleware(handler);
--- utils/auth/firebaseAdmin.js
+++ utils/auth/firebaseAdmin.js
@@ -1,19 +1,21 @@
 import * as admin from 'firebase-admin';
 
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+export const db = admin.firestore();
+
 export const verifyIdToken = (token) => {
-  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
-      }),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-    });
-  }
-
   return admin
     .auth()
     .verifyIdToken(token)
