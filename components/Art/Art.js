5d4a884200d976a7badc07ae435da5d87d8077a7
An Phan <antphan98@gmail.com>
2020-06-12 17:15:23 -0500

    added prices and images to art api

--- components/Art/Art.js
+++ components/Art/Art.js
@@ -1,22 +1,16 @@
 export default ({ art }) => (
   <li>
     <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Forgery</th>
-          <th>Description</th>
-          <th>Image</th>
-        </tr>
-      </thead>
       <tbody>
         <tr>
-          <td>{art.name}</td>
-          <td>{art.hasForgery}</td>
-          <td>{art.desc}</td>
           <td>
-            <img src={art.imageLink}></img>
+            <img src={art.image_uri}></img>
           </td>
+          <td>{art.name}</td>
+          <td>{art.desc}</td>
+
+          <td>{art.buy_price}</td>
+          <td>{art.sell_price}</td>
         </tr>
       </tbody>
     </table>
