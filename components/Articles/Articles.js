3a498f7cb136ed39182a03118c73e4476bcbc393
An Phan <antphan98@gmail.com>
2020-06-29 14:49:42 -0500

    modified articles layout

--- components/Articles/Articles.js
+++ components/Articles/Articles.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { useState, useEffect } from 'react';
-import { Card, Image, Grid, Segment } from 'semantic-ui-react';
+import { Container, Header, Image, Grid } from 'semantic-ui-react';
 
 function Articles() {
   const [error, setError] = useState(null);
@@ -28,31 +28,54 @@ function Articles() {
     return null;
   } else {
     return (
-      <div>
-        <Grid columns="equal">
-          <Grid.Row>
-            <Grid.Column>
-              {results.map((result) => (
-                <Segment key={result}>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header key={result.title}>
-                        {result.title}
-                      </Card.Header>
-                      <Card.Meta>
-                        <span>Written by: {result.authors}</span>
-                      </Card.Meta>
-                      <Image
-                        key={result.image_square_small}
-                        src={result.image.square_small}
-                      />
-                    </Card.Content>
-                  </Card>
-                </Segment>
-              ))}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+      // <div>
+      //   <Grid columns="equal">
+      //     <Grid.Row>
+      //       <Grid.Column>
+      //         {results.map((result) => (
+      //           <Segment key={result}>
+      //             <Card>
+      //               <Card.Content>
+      //                 <Card.Header key={result.title}>
+      //                   {result.title}
+      //                 </Card.Header>
+      //                 <Card.Meta>
+      //                   <span>Written by: {result.authors}</span>
+      //                 </Card.Meta>
+      //                 <Image
+      //                   key={result.image_square_small}
+      //                   src={result.image.square_small}
+      //                 />
+      //               </Card.Content>
+      //             </Card>
+      //           </Segment>
+      //         ))}
+      //       </Grid.Column>
+      //     </Grid.Row>
+      //   </Grid>
+      // </div>
+      <div className="parent">
+        <Container>
+          {results.map((result) => (
+            <Grid celled key={result}>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Image src={result.image.original} />
+                </Grid.Column>
+
+                <Grid.Column width={13}>
+                  <Header>{result.title}</Header>
+                  <span>Written by: {result.authors}</span>
+                  <p>
+                    Publish date:
+                    {result.publish_date}
+                  </p>
+                  <p>{result.deck}</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          ))}
+        </Container>
       </div>
     );
   }
