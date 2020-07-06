874a405b0bcebe4038b146a5fe035840cfeb2fab
An Phan <antphan98@gmail.com>
2020-07-06 10:25:31 -0500

    fixed diy materials list

--- components/DiyItem/DiyItem.js
+++ components/DiyItem/DiyItem.js
@@ -8,32 +8,44 @@ export default ({ diy }) => (
         <img width="90" src={diy.Item_Image}></img>
       </Table.Cell>
       <Table.Cell>{_.startCase(_.toLower(diy.Name))}</Table.Cell>
-      <Table.Cell>
-        {diy.Material_1_count} {diy.Material_1}
-      </Table.Cell>
-      <Table.Cell>
-        {diy.Material_2_count} {diy.Material_2}
+      <Table.Cell className="materials">
+        <div className="material1">
+          {diy.Material_1_count} {diy.Material_1}
+        </div>
+        <div className="material2">
+          {diy.Material_2_count} {diy.Material_2}
+        </div>
+
+        <div className="material3">
+          {diy.Material_3_count} {diy.Material_3}
+        </div>
+
+        <div className="material4">
+          {diy.Material_4_count} {diy.Material_4}
+        </div>
       </Table.Cell>
 
-      <Table.Cell>
-        {diy.Material_3_count} {diy.Material_3}
-      </Table.Cell>
-      <Table.Cell>
-        {diy.Material_4_count} {diy.Material_4}
-      </Table.Cell>
       <Table.Cell>{diy.Category}</Table.Cell>
     </Table.Row>
 
     <style jsx global>{`
+      .ui.table tr td,
+      .ui.table {
+        border-top: none;
+        border: none;
+      }
 
- .ui.table tr td,
- .ui.table {
-   border-top: none;
-   border: none;
- }
+      .ui.table {
+        border-radius: 20px;
+      }
 
- .ui.table {
-   border-radius: 20px;
-`}</style>
+      .material1,
+      .material2,
+      .material3,
+      .material4 {
+        display: flex;
+        flex-direction: column;
+      }
+    `}</style>
   </>
 );
