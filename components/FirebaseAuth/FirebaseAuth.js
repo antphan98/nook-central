76e8633bd4d549e85ad7b47d24ad3aadab03e0dc
An Phan <antphan98@gmail.com>
2020-07-06 13:41:05 -0500

    finished ui for sign in page

--- components/FirebaseAuth/FirebaseAuth.js
+++ components/FirebaseAuth/FirebaseAuth.js
@@ -38,6 +38,69 @@ const FirebaseAuth = () => {
           firebaseAuth={firebase.auth()}
         />
       ) : null}
+      <style jsx global>
+        {`
+          @font-face {
+            font-family: FinkHeavy;
+            src: url(fonts/FinkHeavy.otf) format('opentype');
+          }
+          @font-face {
+            font-family: Humming;
+            src: url('fonts/Humming.otf') format('opentype');
+          }
+          h1.firebaseui-title {
+            font-family: FinkHeavy;
+            text-align: center;
+          }
+          #firebaseui_container {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+          }
+          * {
+            font-family: Humming;
+          }
+          input.firebaseui-input,
+          input.firebaseui-input-invalid {
+            font-family: Humming !important;
+            line-height: 0;
+          }
+
+          .mdl-textfield__label {
+            position: none;
+          }
+          .mdl-button--raised.mdl-button--colored {
+            border-radius: 25px;
+            border-color: brown;
+            border-style: solid;
+            border-width: 0 3px 3px 0;
+            box-shadow: 1px 5px #888888;
+            font-size: 1.5rem;
+            color: white;
+            font-family: FinkHeavy;
+            margin: 20px;
+          }
+          .mdl-button--raised.mdl-button--colored:hover,
+          .mdl-button--primary.mdl-button--primary:hover {
+            transform: scale(0.95) !important;
+            box-shadow: 1px 5px rgba(0, 0, 0, 0.24) !important;
+            z-index: 999;
+          }
+
+          .mdl-button--primary.mdl-button--primary {
+            border-radius: 25px;
+            border-color: brown;
+            border-style: solid;
+            border-width: 0 3px 3px 0;
+            box-shadow: 1px 5px #888888;
+            font-size: 1.5rem;
+            color: #3f51b5;
+            font-family: FinkHeavy;
+          }
+        `}
+      </style>
     </div>
   );
 };
--- pages/auth.js
+++ pages/auth.js
@@ -3,10 +3,17 @@ import FirebaseAuth from '../components/FirebaseAuth/FirebaseAuth';
 const Auth = () => {
   return (
     <div>
-      <p>Sign in</p>
       <div>
         <FirebaseAuth />
       </div>
+      <style jsx global>{`
+        body {
+          background-image: url(images/signin.png);
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+        }
+      `}</style>
     </div>
   );
 };
